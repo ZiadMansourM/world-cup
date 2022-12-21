5477d5d46d67a6614d47a75911ba48ed738b2124
ZiadMansourM <ziadmansour.4.9.2000@gmail.com>
2022-12-21 14:33:11 +0200

    ➕ ADD: initial templates

--- qatar/main/views.py
+++ qatar/main/views.py
@@ -1,8 +1,11 @@
 from django import http
+from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return http.HttpResponse("Hello World")
+    return render(request,'main/home.html',{
+        'title':'home'
+    })
 
 def about(request):
     return http.HttpResponse("About Page")
