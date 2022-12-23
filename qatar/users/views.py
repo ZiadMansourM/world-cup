cdd99166655b0491019de42bd1f045e3b38bd29d
ZiadMansourM <ziadmansour.4.9.2000@gmail.com>
2022-12-23 15:58:26 +0200

    ➕ ADD: register

--- qatar/users/views.py
+++ qatar/users/views.py
@@ -3,7 +3,8 @@ from django.contrib import messages
 from django.contrib.auth import (
     authenticate, 
     login as auth_login,
-    logout as auth_logout
+    logout as auth_logout,
+    get_user_model
 )
 
 # Create your views here.
@@ -11,7 +12,7 @@ def login(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(request, username=username.lower(), password=password)
         if user is not None:
             auth_login(request, user)
             return redirect('home')
@@ -23,6 +24,24 @@ def login(request):
 
 
 def register(request):
+    if request.method == 'POST':
+        password_one = request.POST['password-one']
+        password_two = request.POST['password-two']
+        if password_one == password_two:
+            username = request.POST['username']
+            User = get_user_model()
+            if User.objects.filter(username=username.lower()).exists():
+                messages.error(request, 'Username already exists!')
+            else:
+                user = User.objects.create_user(
+                    username=username.lower(),
+                    password=password_one
+                )
+                user.save()
+                messages.success(request, 'You are now registered and can log in ^^')
+                return redirect('login')
+        else:
+            messages.error(request, 'Passwords do not match!')
     return render(request, 'users/register.html', {
         'title': 'Register',
         'page': 'register'
