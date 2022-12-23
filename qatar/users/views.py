9ea45ae559b0c6ebf7188e5bd57eeaa89bf6b832
ZiadMansourM <ziadmansour.4.9.2000@gmail.com>
2022-12-23 15:05:57 +0200

    ➕ ADD: login

--- qatar/users/views.py
+++ qatar/users/views.py
@@ -1,9 +1,19 @@
-from django import http
-
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login as auth_login
 
 # Create your views here.
 def login(request):
-    return http.HttpResponse("Login Page")
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect('home')
+        messages.error(request, 'Wrong Credentials! Please try again...')
+    return render(request, 'users/login.html')
+
 
 def register(request):
-    return http.HttpResponse("Register Page")
+    return render(request, 'users/register.html')
