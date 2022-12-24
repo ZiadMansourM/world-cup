7018c8a2ec9da334d7e602e599b4ecb96360e8e1
ZiadMansourM <ziadmansour.4.9.2000@gmail.com>
2022-12-24 12:23:04 +0200

    🔧 FIX: Tickets UI

--- qatar/main/views.py
+++ qatar/main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from main.models import Match
+from main.models import Match, Ticket
 
 # Create your views here.
 def home(request):
@@ -10,8 +10,14 @@ def home(request):
 
 def match_list(request):
     matches = Match.objects.all()
+    tickets = Ticket.objects.all()
+    for match in matches:
+        tickets = Ticket.objects.filter(match=match)
+        match.taken_rows = [ticket.seat.row for ticket in tickets]
+        match.taken_cols = [ticket.seat.seat for ticket in tickets]
     return render(request,'main/match-list.html', {
         'matches': matches,
+        'tickets': tickets,
         'title': 'Matches',
         'page': 'matches'
     })
