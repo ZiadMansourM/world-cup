9ac27a11876546a923dd35983f143003b8d04479
ZiadMansourM <ziadmansour.4.9.2000@gmail.com>
2022-12-28 21:39:44 +0200

    🔧 FIX: allowed hosts

--- qatar/qatar/settings.py
+++ qatar/qatar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ag**b(64brm%&vtk^9+doxit$!in98lx#@n5zveyz6pi$2ewn0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
